3f557894f313a6835941309f06dc34a86f79e1b2
yousefa7med <youssefahmedserag@gmail.com>
2025-07-12 14:28:57 +0300

    Add files via upload

---
+++ D. Fixed Password/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num=0;
+    while(true){
+        cin>>num;
+        if(num!=1999){
+            cout<< "Wrong"<<endl;
+        }else{cout<< "Correct"<<endl;
+        break;}
+    }
+}
