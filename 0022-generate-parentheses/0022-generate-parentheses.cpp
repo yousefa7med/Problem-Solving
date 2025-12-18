755594b6b92a8f0236a8f15ae6d1e25b9e619626
yousefa7med <youssefahmedserag@gmail.com>
2025-12-18 07:16:29 +0200

    Time: 3 ms (71.37%), Space: 16 MB (24.03%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,24 +2,49 @@ class Solution {
 
 public:
     vector<string> result;
-void backTrack(string current,int open, int close , int n  ){
-if(current.size()==2*n){
-    result.push_back(current);
+
+void backTrack(int n, int open,int close,string s){
+if(s.size()==2*n)
+{
+
+    result.push_back(s);
     return;
 }
 if(open<n){
-    backTrack(current+"(",open+1,close,n);
+backTrack(n,open+1,close,s+"(");
+
 }
 if(close<open){
-    backTrack(current+")",open,close+1,n);
+    backTrack(n,open,close+1,s+")");
+
 }
 
 
+
 }
+
+
+
+
+
+// void backTrack(string current,int open, int close , int n  ){
+// if(current.size()==2*n){
+//     result.push_back(current);
+//     return;
+// }
+// if(open<n){
+//     backTrack(current+"(",open+1,close,n);
+// }
+// if(close<open){
+//     backTrack(current+")",open,close+1,n);
+// }
+
+
+// }
 
 
     vector<string> generateParenthesis(int n) {
-backTrack("",0,0,n);
+backTrack(n,0,0,"");
 return result;
 
     }
